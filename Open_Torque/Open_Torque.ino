b4d524f8ba294b5de851c205cb6a212318e165ad
Paul G <paul.gaudre@protonmail.com>
2021-02-06 02:11:51 +0100

    first lines of code

--- Open_Torque/Open_Torque.ino
+++ Open_Torque/Open_Torque.ino
@@ -1,9 +1,34 @@
+//-----------------LIBRARY-----------------
+
+#include Calcul.h
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+//-----------------VARIABLE----------------
+
+//OLED
+
+#define SREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+
+//-----------------INIT---------------------
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 void setup() {
-  // put your setup code here, to run once:
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OpenTorque
-Open source torque wrench. 
+Open source torque wrench based on a 50kg load cell sensor, hx711 board, oled screen and a seeduino XIAO.
+
+The electronic is in the handle of the wrench and you can choose what lenght the lever to tight screws up to 400 n/m.
+ 
